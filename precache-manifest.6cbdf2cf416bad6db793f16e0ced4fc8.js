509cd70e92d4e38fa312d0382e319899f8acef96
Burt <2902978956@qq.com>
2020-08-24 11:00:26 +0800

    deploy

---
+++ precache-manifest.6cbdf2cf416bad6db793f16e0ced4fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b74b7b5142a47249858",
+    "url": "/money-website/css/app.89b98508.css"
+  },
+  {
+    "revision": "d5ad557736b12c24dcb69238c310636a",
+    "url": "/money-website/img/QRcode.d5ad5577.png"
+  },
+  {
+    "revision": "140a1a6ae75860065b1104f2a98bc5a4",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "7b74b7b5142a47249858",
+    "url": "/money-website/js/app.859a4e65.js"
+  },
+  {
+    "revision": "e2314f1018357ed34846",
+    "url": "/money-website/js/chunk-vendors.eb6eb628.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
